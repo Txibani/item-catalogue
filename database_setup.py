2f139ebb69efc9bcf7212cf252ae1c496a3f8851
Maite Gracia <maitegracia223@gmail.com>
2018-10-22 19:13:38 +0100

    PIP8 database_setup.py & webserver created

--- database_setup.py
+++ database_setup.py
@@ -7,18 +7,19 @@ from sqlalchemy import create_engine
 
 Base = declarative_base()
 
+
 class Category(Base):
     __tablename__ = 'category'
 
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
 
 
 class CategoryItem(Base):
     __tablename__ = 'category_item'
 
-    name = Column(String(80), nullable = False)
-    id = Column(Integer, primary_key = True)
+    name = Column(String(80), nullable=False)
+    id = Column(Integer, primary_key=True)
     description = Column(String(250))
     category_id = Column(Integer, ForeignKey('category.id'))
     category = relationship(Category)
